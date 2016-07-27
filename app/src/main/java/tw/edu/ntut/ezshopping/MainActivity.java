5511681da13b7c480cc93f105d9c0d4aa1ef95fc
MOONYAN <1787hz@gmail.com>
2016-07-27 17:33:45 +0800

    main page layout

--- app/src/main/java/tw/edu/ntut/ezshopping/MainActivity.java
+++ app/src/main/java/tw/edu/ntut/ezshopping/MainActivity.java
@@ -2,6 +2,7 @@ package tw.edu.ntut.ezshopping;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +12,21 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void scanOnClick(View view)
+    {
+    }
+
+    public void cartOnClick(View view)
+    {
+    }
+
+    public void logOnClick(View view)
+    {
+    }
+
+    public void checkoutOnClick(View view)
+    {
     }
 }
