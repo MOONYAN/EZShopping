0514655603c78b122c5672639863d5060e5d42f2
MOONYAN <1787hz@gmail.com>
2016-08-03 19:25:42 +0800

    fix checkout page

--- app/src/main/java/tw/edu/ntut/ezshopping/CheckoutActivity.java
+++ app/src/main/java/tw/edu/ntut/ezshopping/CheckoutActivity.java
@@ -2,6 +2,8 @@ package tw.edu.ntut.ezshopping;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -10,7 +12,11 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.List;
+
 import tw.edu.ntut.ezshopping.FireField.FireRecord;
+import tw.edu.ntut.ezshopping.ModelField.Cart;
+import tw.edu.ntut.ezshopping.ModelField.CartItem;
 import tw.edu.ntut.ezshopping.ModelField.FireFactory;
 import tw.edu.ntut.ezshopping.ModelField.Model;
 import tw.edu.ntut.ezshopping.ModelField.Record;
@@ -18,10 +24,10 @@ import tw.edu.ntut.ezshopping.ModelField.Record;
 public class CheckoutActivity extends BaseActivity
 {
     private static final String TAG = "CheckoutActivity";
-    private TextView _uidText;
     private TextView _totalText;
+    private RecyclerView _recyclerView;
     private Model _model;
-    private int _total;
+    private Cart _cart;
     private String _uid;
 
     @Override
@@ -32,6 +38,7 @@ public class CheckoutActivity extends BaseActivity
 
         processViews();
         initializeField();
+        initializeRecyclerView();
         updateUI();
     }
 
@@ -40,18 +47,25 @@ public class CheckoutActivity extends BaseActivity
         _model = Model.getInstance();
         SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.preferenceName), MODE_PRIVATE);
         _uid = sharedPreferences.getString("FirebaseId", null);
-        _total = _model.getCart().getTotalCost();
+        _cart = _model.getCart();
+    }
+
+    private void initializeRecyclerView()
+    {
+        final List<CartItem> list = _cart.getItemList();
+        ItemAdapter _adapter = new ItemAdapter(list);
+        _recyclerView.setAdapter(_adapter);
+        _recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     private void updateUI()
     {
-        _uidText.setText(_uid);
-        _totalText.setText(_total + "");
+        _totalText.setText(_cart.getTotalCost() +"");
     }
 
     private void processViews()
     {
-        _uidText = (TextView) findViewById(R.id.uid_text);
+        _recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         _totalText = (TextView) findViewById(R.id.total_text);
     }
 
@@ -78,56 +92,5 @@ public class CheckoutActivity extends BaseActivity
                 finish();
             }
         });
-
-//        CartItem cartItem = new CartItem();
-//        cartItem.ProductId = "TestProductId";
-//        cartItem.Name = "testProduct";
-//        cartItem.ImageURL = "testImage";
-//        cartItem.UnitPrice = 100;
-//        cartItem.Count = 8;
-//        cartItem.Subtotal = 800;
-
-//        List<Product> list = new ArrayList<>();
-//        Product product = new Product();
-//        product.Name = "testName";
-//        product.UnitPrice = 10;
-//        product.ImageURL = "testURL";
-//        list.add(product);
-//        list.add(product);
-//        reference.setValue(list, new DatabaseReference.CompletionListener()
-//        {
-//            @Override
-//            public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference)
-//            {
-//                Log.d(TAG, "onComplete: GGGGGGGGGGGG" );
-//            }
-//        });
-
-//        hideProgressDialog();
-
-
-//        reference.runTransaction(new Transaction.Handler()
-//        {
-//            @Override
-//            public Transaction.Result doTransaction(MutableData mutableData)
-//            {
-//                Product product = mutableData.getValue(Product.class);
-//                product.Name = "testName";
-//                product.UnitPrice = 10;
-//                product.ImageURL = "testURL";
-//                product.Subtotal = 100;
-//                product.Count = 10;
-//                mutableData.setValue(product);
-//                return null;
-//            }
-//
-//            @Override
-//            public void onComplete(DatabaseError databaseError, boolean b, DataSnapshot dataSnapshot)
-//            {
-////                hideProgressDialog();
-////                _model.setNewCart();
-////                finish();
-//            }
-//        });
     }
 }
