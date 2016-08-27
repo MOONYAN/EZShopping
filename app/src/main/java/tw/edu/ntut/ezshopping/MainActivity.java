502595058c51412b032b1c8b913cb302d97a3b45
MOONYAN <1787hz@gmail.com>
2016-08-27 23:21:16 +0800

    dm mail call

--- app/src/main/java/tw/edu/ntut/ezshopping/MainActivity.java
+++ app/src/main/java/tw/edu/ntut/ezshopping/MainActivity.java
@@ -2,6 +2,7 @@ package tw.edu.ntut.ezshopping;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -65,6 +66,30 @@ public class MainActivity extends AppCompatActivity
     public void accountOnClick(View view)
     {
         Intent intent = new Intent(this, AccountActivity.class);
+        startActivity(intent);
+    }
+
+    public void mailOnClick(View view)
+    {
+//        Intent intent = new Intent(Intent.ACTION_SEND);
+//        intent.setType("plain/text");
+//        intent.putExtra(Intent.EXTRA_EMAIL,new String[]{"t102590043@ntut.org.tw"});
+        Intent intent = new Intent(Intent.ACTION_SENDTO,Uri.parse("mailto:t102590043@ntut.org.tw"));
+        intent.putExtra(Intent.EXTRA_TEXT,"test email body");
+        intent.putExtra(Intent.EXTRA_SUBJECT,"contact to ez-service");
+//        startActivity(Intent.createChooser(intent,"sending email"));
+        startActivity(intent);
+    }
+
+    public void dmOnClick(View view)
+    {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://owen-ez-store.herokuapp.com/ListItem.html"));
+        startActivity(intent);
+    }
+
+    public void callOnClick(View view)
+    {
+        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:117"));
         startActivity(intent);
     }
 }
